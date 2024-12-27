898b17c9018a468508c655fdfade5f8dc706df74
Praveen Gupta <praveenguptaa8@gmail.com>
2024-12-27 22:56:25 +0530

    Minor fixes

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -38,11 +38,14 @@ class CityRepository {
 
     async updateCity(cityId,data){
         try {
-            const city = await City.update(data,{
-                where: {
-                    id : cityId
-                }
-            });
+            // const city = await City.update(data,{
+            //     where: {
+            //         id : cityId
+            //     }
+            // });
+            const city = await City.findByPk(cityId);
+            city.name = data.name;
+            await city.save();
             return city;
         } catch (error) {
             console.log("Something went wrong in city-repository layer");
